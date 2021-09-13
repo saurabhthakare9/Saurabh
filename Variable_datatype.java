d9e73017cc950d0dd0e8b78dc751c80040df0e14
saurabhthakare9 <90328247+saurabhthakare9@users.noreply.github.com>
2021-09-14 01:03:18 +0530

    var datatype

---
+++ Variable_datatype.java
@@ -0,0 +1,52 @@
+package datatypes;
+
+
+public class Variable_datatype {
+
+	public static void main (String [] args)
+	{
+		
+		String a= "virat";
+		String b= "kohli";
+		int abc =15;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(abc);
+		
+		/*	method1();
+			Variable_datatype ddd= new Variable_datatype();
+			ddd.method2();
+	    */
+	
+	}	
+	public static void method1()
+	{
+		String a = "messi";            //we can also do variable & datatypes in  main method but for 
+		int no=10;						//we are using static & non static methods also
+		String country="argentina";
+		String club = "FCB";
+		int c=32;
+		System.out.println(a);
+		System.out.println(no);
+		System.out.println(country);
+		System.out.println(club);
+	System.out.println(c);
+		
+		
+		
+	}
+	public void method2()
+	{
+		String name="Saurabh";
+		int birthday = 24;
+		int age = 29;
+		String surname ="thakare";
+		String city="Buldana";
+		
+		System.out.println(name);
+		System.out.println(birthday);
+		System.out.println(age); 
+		System.out.println(surname);
+		System.out.println(city);
+	}
+}
